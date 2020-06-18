8036ffdec85b48169b7e7f17ac169c6f0ebf508d
EduardoPazini <eduardopazzzini@gmail.com>
2020-06-18 16:54:21 -0300

    get creation data and segment

--- jiraConnection.py
+++ jiraConnection.py
@@ -6,7 +6,7 @@ import requests
 import os
 
 # Set here the path of your .env
-env_path = Path('/home/eduardo')/'.env'
+env_path = Path('/home/ruivo')/'.env'
 load_dotenv(dotenv_path=env_path)
 
 workspace = os.getenv("WORKSPACE")
@@ -22,10 +22,9 @@ class Config():
         for issue in self.jira.search_issues('project=CLIEN ORDER BY key ASC', startAt=0, maxResults=None):
             key = (int(issue.key.split('-',1)[1]))
             if(key >= initial and key <= final):
-                if(issue.fields.description != None):
-                    psw = get_password(issue.fields.description)
-                    if (psw != -1):
-                        infos = infos + structure_response(str(key), psw)
+                created = issue.fields.created
+                seg = issue.fields.customfield_10029
+                infos = infos + structure_response(str(key), str(created), str(seg))
         archive(infos)
 
     def swap(self, args):
@@ -52,12 +51,12 @@ def get_password(description):
         return -1
     return psw
 
-def structure_response(clien, psw):
-    lineInfo = str('CLIEN-' + clien + ', ' + psw + '\n')
+def structure_response(clien, created, seg):
+    lineInfo = str('CLIEN-' + clien + ', ' + created + ', ' + seg +'\n')
     return lineInfo
 
 def archive(infos):
-    file = 'infos.txt'
+    file = 'infos.csv'
     try:
       with open(file, 'a') as f:
            f.write(infos)
